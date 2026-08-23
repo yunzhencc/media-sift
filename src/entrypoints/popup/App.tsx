6394450627a389a3003ac2c8dfc4071d8bda82bb
wangxingkang <wang_xingkang@qq.com>
2026-08-24 00:57:25 +0800

    feat: add separator component

--- src/entrypoints/popup/App.tsx
+++ src/entrypoints/popup/App.tsx
@@ -1,40 +1,13 @@
-import { useState } from 'react'
-import reactLogo from '@/assets/react.svg'
-import './App.css'
-import wxtLogo from '/wxt.svg'
+import { Button } from '@/components/ui/button'
+import { Separator } from '@/components/ui/separator'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://wxt.dev" target="_blank">
-          <img src={wxtLogo} className="logo" alt="WXT logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+    <div className="w-60">
       <h1>WXT + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is
-          {' '}
-          {count}
-        </button>
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the WXT and React logos to learn more
-      </p>
-    </>
+      <Separator />
+      <Button>Button</Button>
+    </div>
   )
 }
 
