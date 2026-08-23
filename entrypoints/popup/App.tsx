adb0f3f1b29818c346aa3a03aa970c85f53fa9d6
wangxingkang <wang_xingkang@qq.com>
2026-08-23 23:58:30 +0800

    chore: configure eslint

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
-import reactLogo from '@/assets/react.svg';
-import wxtLogo from '/wxt.svg';
-import './App.css';
+import { useState } from 'react'
+import reactLogo from '@/assets/react.svg'
+import './App.css'
+import wxtLogo from '/wxt.svg'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -18,18 +18,24 @@ function App() {
       </div>
       <h1>WXT + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setCount(count => count + 1)}>
+          count is
+          {' '}
+          {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit
+          {' '}
+          <code>src/App.tsx</code>
+          {' '}
+          and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
         Click on the WXT and React logos to learn more
       </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
